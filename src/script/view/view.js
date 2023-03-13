1da11c9d2638f76789dafd0023d97134464e748e
trihuang <124452877+trihuang@users.noreply.github.com>
2023-03-13 18:36:58 +0800

    Implement display todos

--- src/script/view/view.js
+++ src/script/view/view.js
@@ -131,12 +131,15 @@ class View {
         const date = document.createElement('div');
         date.classList.add('text-muted');
         date.classList.add('date');
-        if (isSameDay(item.dueDate, new Date())) {
-            date.textContent = 'Today';
-        } else if (!isSameYear(item.dueDate, new Date())) {
-            date.textContent = format(item.dueDate, 'MMM d yyyy');
-        } else {
-            date.textContent = format(item.dueDate, 'MMM d');
+
+        if (item.dueDate !== undefined) {
+            if (isSameDay(item.dueDate, new Date())) {
+                date.textContent = 'Today';
+            } else if (!isSameYear(item.dueDate, new Date())) {
+                date.textContent = format(item.dueDate, 'MMM d yyyy');
+            } else {
+                date.textContent = format(item.dueDate, 'MMM d');
+            }
         }
         infoContainer.appendChild(date);
 
@@ -197,7 +200,35 @@ class View {
 
         const accordionBody = document.createElement('div');
         accordionBody.classList.add('accordion-body');
-        //TODO
+
+        if (item.description !== '' || item.description !== undefined) {
+            const description = document.createElement('div');
+            description.textContent = item.description;
+            accordionBody.appendChild(description);
+        }
+
+        if (item.notes !== '' || item.notes !== undefined) {
+            const notes = document.createElement('div');
+            notes.classList.add('text-muted');
+            notes.classList.add('notes');
+            notes.textContent = item.notes;
+            accordionBody.appendChild(notes);
+        }
+
+        if (item.todos !== undefined) {
+            let task;
+            for (let i = 0; i < item.todos.length; i++) {
+                task = this.displayTodo(item.todos[i]);
+                accordionBody.appendChild(task);
+
+                if (item.todos[i].todos !== undefined) {
+                    for (let j = 0; j < item.todos[i].todos.length; j++) {
+                        task = this.displayTodo(item.todos[i].todos[j]);
+                        accordionBody.appendChild(task);
+                    }
+                }
+            }
+        }
 
         if (item.isProject) {
             const link = document.createElement('div');
@@ -213,16 +244,103 @@ class View {
         return accordionItem;
     }
 
+    displayTodo(todo) {
+        const projectTodo = document.createElement('div');
+        projectTodo.classList.add('project-todo');
+
+        const title = document.createElement('span');
+        title.classList.add('width');
+        title.classList.add('d-inline-block');
+
+        if (todo.todoParent !== undefined) {
+            title.classList.add('sub-todo');
+        }
+
+        const icon = document.createElement('i');
+        icon.classList.add('bi');
+        icon.classList.add('bi-circle');
+        icon.classList.add('todo-circle');
+        title.appendChild(icon);
+        const name = document.createElement('span');
+        name.textContent = todo.title;
+        title.appendChild(name);
+        projectTodo.appendChild(title);
+
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('d-inline-flex');
+        infoContainer.classList.add('remaining-width');
+        infoContainer.classList.add('justify-content-between');
+
+        const date = document.createElement('span');
+        date.classList.add('text-muted');
+        date.classList.add('due-date');
+
+        if (todo.dueDate !== undefined) {
+            if (isSameDay(todo.dueDate, new Date())) {
+                date.textContent = 'Today';
+            } else if (!isSameYear(todo.dueDate, new Date())) {
+                date.textContent = format(todo.dueDate, 'MMM d yyyy');
+            } else {
+                date.textContent = format(todo.dueDate, 'MMM d');
+            }
+        }
+        infoContainer.appendChild(date);
+
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('buttons-container');
+        buttonsContainer.classList.add('d-flex');
+        buttonsContainer.classList.add('justify-content-end');
+
+        const priorityButton = document.createElement('div');
+        priorityButton.classList.add('status');
+        priorityButton.classList.add('priority');
+        priorityButton.classList.add('rounded-pill');
+        priorityButton.classList.add('text-light');
+        priorityButton.classList.add('text-center');
+
+        if (todo.priority === 'None') {
+            priorityButton.classList.add('bg-transparent');
+        } else if (todo.priority === 'Medium') {
+            priorityButton.classList.add('md-priority');
+            priorityButton.textContent = 'Medium';
+        } else if (todo.priority === 'High') {
+            priorityButton.classList.add('high-priority');
+            priorityButton.textContent = 'High';
+        }
+        buttonsContainer.appendChild(priorityButton);
+
+        const statusButton = document.createElement('div');
+        statusButton.classList.add('status');
+        statusButton.classList.add('rounded-pill');
+        statusButton.classList.add('text-light');
+        statusButton.classList.add('text-center');
+        statusButton.classList.add('d-inline-block');
+        statusButton.classList.add('ml-auto');
+
+        if (todo.status === 'None') {
+            statusButton.classList.add('bg-transparent');
+        } else if (todo.status === 'Overdue') {
+            statusButton.classList.add('bg-danger');
+            statusButton.textContent = 'Overdue';
+        } else if (todo.status === 'In Progress') {
+            statusButton.classList.add('bg-info');
+            statusButton.textContent = 'In Progress';
+        } else if (todo.status === 'Completed') {
+            statusButton.classList.add('bg-success');
+            statusButton.textContent = 'Completed';
+        }
+        buttonsContainer.appendChild(statusButton);
+        infoContainer.appendChild(buttonsContainer);
+        projectTodo.appendChild(infoContainer);
+        return projectTodo;
+    }
+
     displayTodosHeader(projectTitle) {
         const header = document.querySelector('.navbar > h2');
-        header.textContent = `${projectTitle}`;
+        header.textContent = projectTitle;
     }
 
     displayTodos(todos) {
-        // TODO
-    }
-
-    displayTodo(todo) {
         // TODO
     }
 
